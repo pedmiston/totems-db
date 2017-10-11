a94a7049f18629488077d0912f53da9ed9f261b2
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-10-11 08:36:43 -0500

    Moved scrubber in to invoke tasks

--- db.py
+++ db.py
@@ -2,6 +2,8 @@
 from os import environ
 
 import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
 import ansible_vault
 from unipath import Path
 import fire
@@ -11,6 +13,47 @@ from models import Group, Player
 
 
 PROJ = Path(__file__).absolute().parent
+
+class DB:
+    def __init__(self):
+        self._engine = connect_to_db()
+        self._sessionmaker = sessionmaker(bind=self._engine)
+
+    def remove_player(self, player_id):
+        session = self._sessionmaker()
+        player = self._query_player(player_id, _session=session)
+        player.delete()
+        session.commit()
+
+    def verify_player(self, player_id):
+        return self._query_player(player_id) != None
+
+    def set_group_open(self, group_id, open=False):
+        session = self._sessionmaker()
+        group = self._query_team(group_id, _session=session)
+        group.Open = open
+        session.commit()
+
+    def set_group_size(self, group_id, size):
+        session = self._sessionmaker()
+        group = self._query_team(group_id, _session=session)
+        group.Size = Size
+        session.commit()
+
+    def _query_player(self, player_id, _session=None):
+        session = _session or self._sessionmaker()
+        player = (session.query(Player)
+                         .filter_by(ID_Player=player_id)
+                         .first())
+        return player
+
+    def _query_team(self, group_id, _session=None):
+        session = _session or self._sessionmaker()
+        group = (session.query(Group)
+                        .filter_by(ID_Group=group_id)
+                        .first())
+        return group
+
 
 def connect_to_db():
     url = "mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}".format(
--- tasks.py
+++ tasks.py
@@ -1,90 +1,46 @@
 from invoke import task
-import pandas
-import db
-from db import Group, Player
-
-from .paths import *
+from db import DB
 
 @task
 def install(ctx):
     """Install the totems database on a remote server."""
-    ctx.run('cd db && ansible-playbook install.yml')
-
+    ctx.run('ansible-playbook install.yml')
 
 @task
 def snapshot(ctx):
     """Take a snapshot of the totems database."""
-    ctx.run('cd db && ansible-playbook snapshot.yml')
-
+    ctx.run('ansible-playbook snapshot.yml')
 
 @task
 def restore(ctx, dump):
     """Restore the totems database from a snapshot."""
-    cmd = 'cd db && ansible-playbook restore.yml -e dump={dump}'
+    cmd = 'ansible-playbook restore.yml -e dump={dump}'
     ctx.run(cmd.format(dump=dump))
 
+@task
+def verify_player(ctx, player_id):
+    db = DB()
+    exists = db.verify_player(player_id)
+    print('ID_Player={}: {}'.format(player_id, exists))
 
 @task
-def label(ctx, subj_info=None):
-    """Label valid subjects (modifies Table_Group)."""
-    con = db.connect_to_db()
-    players = pandas.read_sql('SELECT * FROM Table_Player', con)
-    groups = pandas.read_sql('SELECT * FROM Table_Group', con)
-    players = players.merge(groups)
-
-    # Verify team sizes
-    actual_sizes = players.groupby('ID_Group').size()
-    actual_sizes.name = 'ActualSize'
-    players = players.merge(actual_sizes.reset_index())
-    players['is_team_full'] = (players.Size == players.ActualSize)
-
-    # Drop any players not in the subject info sheet
-    subj_info = subj_info or Path(TOTEMS_RAW_DATA, 'SubjInfo.csv')
-    if not subj_info.exists():
-        raise AssertionError((
-            'Subject info not found at "{}". To download it, run:'
-            '\n\n\tinv experiment.download -n subj_info\n'
-        ).format(subj_info))
-
-    subj_info = pandas.read_csv(subj_info)
-    players['is_known_player'] = (players.ID_Player
-                                         .astype(int)
-                                         .isin(subj_info.ID_Player))
-
-    # Label those groups with known players and full teams as valid.
-    group_statuses = players.groupby('ID_Group').apply(
-        lambda x: all(x.is_team_full) and all(x.is_known_player)
-    )
-    group_statuses.name = 'is_valid_group'
-    group_statuses = group_statuses.reset_index()
-    group_statuses['Status'] = None
-    group_statuses.Status.where(~group_statuses.is_valid_group, 'E',
-                                inplace=True)
-    del group_statuses['is_valid_group']
-    group_statuses = group_statuses.to_dict('records')
-    group_ids = [group['ID_Group']
-                 for group in group_statuses
-                 if group['Status'] == 'E']
-
-    # Update the database to reflect the valid groups
-    con.execute(Group.update().values(Status = None))
-    con.execute(
-        Group.update()
-             .values(Status = 'E')
-             .where(Group.c.ID_Group.in_(group_ids))
-    )
-
+def toggle_group_open(ctx, group_id):
+    pass
 
 @task
-def close(ctx, group_id):
-    """Close an open group."""
-    con = db.connect_to_db()
+def set_group_size(ctx, group_id, size):
+    pass
 
-    groups = pandas.read_sql('SELECT * FROM Table_Group', con)
-    assert group_id in groups.ID_Group.tolist()
+@task
+def delete_player(ctx, player_id):
+    pass
 
-    con.execute(
-        Group.update()
-             .values(Open = 0)
-             .where(Group.c.ID_Group == group_id)
-    )
+@task
+def run_sql(ctx, command_file):
+    """Run arbitrary SQL commands from a file."""
+    db = DB()
+    cmds = [line.strip() for line in open(command_file).readlines()]
+    for line in open(command_file).readlines():
+        line = line.strip()
+        print('Executing: {}'.format(line))
+        # db._engine.execute(line)
