c7f572368c951768fcc636dd1a5b67873272e7e8
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-10-16 08:51:39 -0500

    Before reseting after power outage

---
+++ reset-after-power-outage.sql
@@ -0,0 +1,8 @@
+DELETE FROM Table_Player WHERE ID_Player = 446
+UPDATE Table_Group  SET Size = 2, Open = 1 WHERE ID_Group = 'G_10/9/2017 12:31:17 PM'
+
+DELETE FROM Table_Player WHERE ID_Player = 444
+UPDATE Table_Group  SET Size = 2, Open = 1 WHERE ID_Group = 'G_10/9/2017 2:06:26 PM'
+
+DELETE FROM Table_Player WHERE ID_Player = 445
+UPDATE Table_Group SET Size = 3, Open = 1 WHERE ID_Group = 'G_10/9/2017 2:30:09 PM'
