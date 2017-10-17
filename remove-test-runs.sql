94f246e952c76469d6814d5fc600ca048f0aa1ee
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-10-17 10:36:01 -0500

    Tested and then cleaned everything up

---
+++ remove-test-runs.sql
@@ -0,0 +1,3 @@
+DELETE FROM Table_Player WHERE ID_Player IN (473, 474)
+UPDATE Table_Group  SET Size = 1, Open = 0 where ID_Group = 'G_10/16/2017 3:00:04 PM'
+UPDATE Table_Group  SET Size = 3, Open = 0 where ID_Group = 'G_10/9/2017 2:06:26 PM'
