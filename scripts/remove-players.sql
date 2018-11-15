1740587eab07222a6d7600991699ddf1c422295e
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-11-15 09:49:03 -0600

    Remove players from when inheritance was wrong

--- scripts/remove-players.sql
+++ scripts/remove-players.sql
@@ -1,4 +1,1 @@
-DELETE FROM Table_Player WHERE ID_Player IN (878, 879, 880)
-UPDATE Table_Group SET Size = 5 WHERE ID_Group = 'G_10/9/2017 1:06:42 PM'
-DELETE FROM Table_Player WHERE ID_Player IN (868, 869)
-UPDATE Table_Group SET Size = 6 WHERE ID_Group = 'G_9/18/2018 11:03:34 AM'
+DELETE FROM Table_Player WHERE ID_Player IN (846, 853, 851, 852, 850, 860, 861)
